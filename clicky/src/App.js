d5f5c6b044cc7a6a1ea6a97024f2ddae8947e0f7
Samuel Meyer <svmeyer@Samuels-MacBook-Pro.local>
2019-11-14 21:33:15 -0800

    adding to home page for functionality

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Navbar from "./components/navbar"
-import Game from "./components/card"
 import './App.css';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Game />
+      <Card />
     </div>
   );
 }
--- clicky/src/pages/Home.js
+++ clicky/src/pages/Home.js
@@ -1,5 +1,14 @@
 import React, { Component } from "react";
+import images from 
 
 class Home extends Component {
-  
+const 
+
+  state = {
+    image: []
+  };
+
+  handleClick = event => {
+  }
+
 }
